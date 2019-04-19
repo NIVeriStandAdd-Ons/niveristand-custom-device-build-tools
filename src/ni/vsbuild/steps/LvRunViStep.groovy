9aa18b503ff100f15af92ffe64314d3359f3460c
Michael Bilyk <Michael.Bilyk@ni.com>
2019-04-19 18:14:50 -0500

    Add option to run VI unsecure

--- src/ni/vsbuild/steps/LvRunViStep.groovy
+++ src/ni/vsbuild/steps/LvRunViStep.groovy
@@ -5,13 +5,15 @@ import ni.vsbuild.BuildConfiguration
 class LvRunViStep extends LvStep {
 
    def vi
+   def option
 
    LvRunViStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
       this.vi = mapStep.get('vi')
+      this.option = mapStep.get('option')
    }
 
    void executeStep(BuildConfiguration configuration) {
-      script.lvRunVi(vi, lvVersion)
+      script.lvRunVi(vi, option, lvVersion)
    }
 }
--- vars/lvRunVi.groovy
+++ vars/lvRunVi.groovy
@@ -1,5 +1,10 @@
-def call(vi, lvVersion){
+def call(vi, option, lvVersion){
    echo "Running $vi."
    def logFileName = getLogName(vi)
-   labviewcli("-OperationName SecureRunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
+   if (option == 1) {
+     def versionPath = env."labviewPath_${lvVersion}"
+     bat "LabVIEWCLI -LabVIEWPath \"${versionPath}\" -OperationName RunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\""
+   } else {
+     labviewcli("-OperationName SecureRunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
+   }
 }
